d9e8bb379b71e45a0e5df07ed16a076eb836ee4e
Antoine Cœur <coeur@gmx.fr>
2017-02-20 12:03:08 +0800

    Initial Commit

---
+++ SimonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Antoine Cœur on 20/02/2017.
+//  Copyright © 2017 Cœur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
